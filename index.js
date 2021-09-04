ee5be1ec4f4d03dbabcb94e818cceb8c6da4a009
Samuel-DeSantis <samuel.i.desantis@gmail.com>
2021-09-04 16:38:16 -0600

    commit tree a573bb0a3913f9f90b2e7658382b67aedddb9426

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Your code here
+function arrayMapping (arr, value = 1) {
+    let newArray = []
+    if (typeof arr[0] === 'string') {
+        arr.forEach ( item => newArray.push(item) )
+    } else {
+        arr.forEach ( item => newArray.push(value * item) )
+    }
+    return newArray
+}
+
+function mapToNegativize (arr) {
+    return arrayMapping(arr, -1)
+}
+
+function mapToNoChange (arr) {
+    return arrayMapping(arr)
+}
+
+function mapToDouble (arr) {
+    return arrayMapping(arr, 2)
+}
+
+function mapToSquare (arr) {
+    let newArray = []
+    arr.forEach ( item => newArray.push(item * item) )
+    return newArray
+}
+
+function reduceToTotal (arr, start=0) {
+    let total = start
+    arr.forEach ( item => total += item )
+    return total
+}
+
+function reduceToAllTrue (arr) {
+    let status = true
+    arr.forEach ( item => status = status && Boolean(item) )
+    return status
+}
+
+function reduceToAnyTrue (arr) {
+    let status = false
+    arr.forEach ( item => status = status || Boolean(item) )
+    return status
+}
